92df2b4145192c4c5d463a83be5b6b2bab18a421
Pragya Snehi <snehipragya@gmail.com>
2026-07-14 13:24:45 +0530

    Time: 131 ms (69.44%) | Memory: 47.2 MB (80.56%) - LeetSync

--- 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.java
+++ 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.java
@@ -1,35 +1,54 @@
 class Solution {
     static final int MOD = 1_000_000_007;
-    static final int MAX = 200;
+
     public int subsequencePairCount(int[] nums) {
-        long[][] dp = new long[MAX + 1][MAX + 1];
+        int m = 0;
+        for (int num : nums) {
+            m = Math.max(m, num);
+        }
+
+        int[][] dp = new int[m + 1][m + 1];
         dp[0][0] = 1;
-        for (int x : nums) {
-            long[][] next = new long[MAX + 1][MAX + 1];
-            for (int g1 = 0; g1 <= MAX; g1++) {
-                for (int g2 = 0; g2 <= MAX; g2++) {
-                    long cur = dp[g1][g2];
-                    if (cur == 0) continue;
-                    next[g1][g2] = (next[g1][g2] + cur) % MOD;
-                    int ng1 = (g1 == 0) ? x : gcd(g1, x);
-                    next[ng1][g2] = (next[ng1][g2] + cur) % MOD;
-                    int ng2 = (g2 == 0) ? x : gcd(g2, x);
-                    next[g1][ng2] = (next[g1][ng2] + cur) % MOD;
+
+        for (int num : nums) {
+            int[][] ndp = new int[m + 1][m + 1];
+
+            for (int j = 0; j <= m; j++) {
+                int divisor1 = gcd(j, num);
+
+                for (int k = 0; k <= m; k++) {
+                    int val = dp[j][k];
+                    if (val == 0) continue;
+
+                    int divisor2 = gcd(k, num);
+
+                    // Skip the current number
+                    ndp[j][k] = (int) ((ndp[j][k] + (long) val) % MOD);
+
+                    // Add to first subsequence
+                    ndp[divisor1][k] = (int) ((ndp[divisor1][k] + (long) val) % MOD);
+
+                    // Add to second subsequence
+                    ndp[j][divisor2] = (int) ((ndp[j][divisor2] + (long) val) % MOD);
                 }
             }
-            dp = next;
+
+            dp = ndp;
         }
-        long ans = 0;
-        for (int g = 1; g <= MAX; g++) {
-            ans = (ans + dp[g][g]) % MOD;
+
+        int ans = 0;
+        for (int j = 1; j <= m; j++) {
+            ans = (int) ((ans + (long) dp[j][j]) % MOD);
         }
-        return (int) ans;
+
+        return ans;
     }
+
     private int gcd(int a, int b) {
         while (b != 0) {
-            int t = a % b;
+            int temp = a % b;
             a = b;
-            b = t;
+            b = temp;
         }
         return a;
     }
